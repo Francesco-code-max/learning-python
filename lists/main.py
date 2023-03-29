e5168963a1611a39a47c5e0ca8eeaee65dced351
Francesco-code-max <francesaroni@icloud.com>
2023-03-29 21:35:38 +0300

    Commented my work

--- lists/main.py
+++ lists/main.py
@@ -1,13 +1,23 @@
-list1 = [1, "hi", "Python", 2]
-print(type(list1))
 
-print(list1)
 
-print(list1[3:])
 
-print(list1[0:2])
 
-print(list1 + list1)
 
-print(list1 * 3) 
+
+
+
+
+
+# list1 = [1, "hi", "Python", 2]
+# print(type(list1))
+
+# print(list1)
+
+# print(list1[3:])
+
+# print(list1[0:2])
+
+# print(list1 + list1)
+
+# print(list1 * 3) 
 
