d30263f384894d504bdc30e22f3096ee29fdd236
Francesco-code-max <francesaroni@icloud.com>
2023-03-30 15:36:56 +0300

    Learned about Remove and Delete

---
+++ delete/main.py
@@ -0,0 +1,15 @@
+# # PYTHON REMOVE AND DELETE 
+
+# # DELETE
+languages = [ 'Python', 'HTML', 'CSS', 'C++', 'C']
+del languages[2]
+print("Here are the languages", languages)
+
+del languages[-1]
+print("This are the remaining languages" , languages)
+
+
+# REMOVE 
+names = ['Francesco', 'Pierina', 'Kevin', 'Anita', 'Noel']
+names.remove('Noel')
+print("This are the names", names)
