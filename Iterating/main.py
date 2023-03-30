e4d4bebb7f3b172b00c02a3106b47eabb4f47276
Francesco-code-max <francesaroni@icloud.com>
2023-03-30 17:48:06 +0300

    Learned iteration and getting length

---
+++ Iterating/main.py
@@ -0,0 +1,6 @@
+names = ['Francesco', 'Cesaroni', 'Kinyanjui']
+for name in names:
+    print(name)
+
+#getting the length of the elements 
+print("Here are the total number of elements: ", len(names))
