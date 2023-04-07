20f5c309e33fd47ad56cc464502a97805edd01f4
Francesco-code-max <francesaroni@icloud.com>
2023-04-07 14:42:38 +0300

    Looping through a list

---
+++ through/main.py
@@ -0,0 +1,8 @@
+#LOOPING THROUGH A LIST
+# thisList = ["Apple", "Banana", "Grapes"]
+# for x in thisList:
+#     print(thisList)
+
+thisList = ["Apple", "Banana", "Grapes"]
+for x in range(len(thisList)):
+    print(thisList[x])
