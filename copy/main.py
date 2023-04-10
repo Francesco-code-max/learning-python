2b99e625f15a6e7aad08fcc128457f1fa089e239
Francesco-code-max <francesaroni@icloud.com>
2023-04-10 14:56:23 +0300

    copied one list to another

---
+++ copy/main.py
@@ -0,0 +1,8 @@
+# thislist = ["apple", "banana", "cherry"]
+# newList = thislist.copy()
+# print(newList)
+
+#Another Way using the List function 
+thislist = ["apple", "banana", "cherry"]
+newList = list(thislist)
+print(newList)
