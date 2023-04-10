d6b678121382eb77419df84d13882c2fd5def6d2
Francesco-code-max <francesaroni@icloud.com>
2023-04-10 14:41:12 +0300

    Sorted Lists

---
+++ sort/main.py
@@ -0,0 +1,14 @@
+#Sorting Alphanumerically
+# fruits = ["orange", "kiwi", "banana", "pineapple", "lemon"]
+# fruits.sort()
+# print(fruits)
+
+#sorting Numerically
+# numbers = [100, 20, 10, 60, 80, 30, 50, 70, 90, 40]
+# numbers.sort()
+# print(numbers)
+
+#Descending order
+numbers = [100, 20, 10, 60, 80, 30, 50, 70, 90, 40]
+numbers.sort(reverse = True)
+print(numbers)
