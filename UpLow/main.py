3ffc417ed8523bcc10ed01e41f5d339cc96ce874
Francesco-code-max <francesaroni@icloud.com>
2023-04-10 14:18:36 +0300

    Changed to Upper and lower case

---
+++ UpLow/main.py
@@ -0,0 +1,9 @@
+#To change to UpperCase
+# name = [ "francesco", "kinyanjui", "cesaroni"]
+# newName = [x.upper() for x in name]
+# print(newName)
+
+# To change to LowerCase
+name = ["FRANCESCO", "CESARONI", "KINYANJUI"]
+newName = [x.lower() for x in name]
+print(newName)
