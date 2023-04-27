ff2fb8f93fa444a1bf2a176d1ccaacd540c12227
Francesco-code-max <francesaroni@icloud.com>
2023-04-27 20:53:05 +0300

    Learned about python dictionaries

---
+++ Dictionaries/main.py
@@ -0,0 +1,52 @@
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# print(thisdict)
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# print(thisdict ["brand"])
+
+# #DETERMINING THE LENGTH 
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# print(len (thisdict))
+
+# #DATA TYPES
+# thisdict = {
+#   "brand": "Ford",
+#   "electric": False,
+#   "year": 1964,
+#   "colors": ["red", "white", "blue"]
+# }
+# print(thisdict)
+
+#TYPE
+# thisdict = {
+#   "brand": "Ford",
+#   "electric": False,
+#   "year": 1964,
+#   "colors": ["red", "white", "blue"]
+# }
+# print(type(thisdict))
+
+#USING THE DICT() CONSTRUCTOR
+# thisdict = dict  (name = "Francesco", age = "22", nationality = "Kenyan")
+# print(thisdict)
+
+#ACCESSING ITEMS
+thisdict = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year" : 2023
+}
+x = thisdict["model"]
+print(x)
